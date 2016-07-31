279314c7e845c5b1bb50bd7ec99dd34499de6ea6
Gilad Goldberg <ggoldberg@paypal.com>
2016-07-31 14:39:06 +0300

    bug fixes

--- middleware/slack.js
+++ middleware/slack.js
@@ -1,18 +1,22 @@
 function slackParams(req, res, next) {
-	req.teamId = req.body.team_id
-	req.teamDomain = req.body.team_domain
-	req.channelId = req.body.channel_id
-	req.channelName = req.body.channel_name
-	req.cmdText = req.body.text
-	req.userId = req.body.user_id
-	req.userName = req.body.user_name
+	if (req.body) {
+		req.teamId = req.body.team_id
+		req.teamDomain = req.body.team_domain
+		req.channelId = req.body.channel_id
+		req.channelName = req.body.channel_name
+		req.cmdText = req.body.text
+		req.userId = req.body.user_id
+		req.userName = req.body.user_name
+	}
 
-	const space = req.cmdText.indexOf(' ')
-	if (space >= 0) {
-		req.cmdName = req.cmdText.substr(0, space)
-		req.cmdArgs = req.cmdText.substr(space + 1)
-	} else {
-		req.cmdName = req.cmdText
+	if (req.cmdText) {
+		const space = req.cmdText.indexOf(' ')
+		if (space >= 0) {
+			req.cmdName = req.cmdText.substr(0, space)
+			req.cmdArgs = req.cmdText.substr(space + 1)
+		} else {
+			req.cmdName = req.cmdText
+		}
 	}
 
 
